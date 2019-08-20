80cd83a8da5275d96aa813a0be5d7f9d16b146c7
Michael Hamilton <michael.hamilton@msquarehealthcare.com>
2019-08-20 10:49:34 -0400

    New methods for grouping fixtures and updating subsets of fixtures

--- example.js
+++ example.js
@@ -3,19 +3,33 @@
 // Require the module
 const dmxus = require('./index');
 
+
 // Create an instance of dmxus with the correct port
 const d = new dmxus('COM6');
 
-// Patch a fixture
+
+// Patch some fixtures
 d.patchFixture( 1, dmxus.getDeviceProfile("IRGB"));
+d.patchFixture( 8, dmxus.getDeviceProfile("IRGB"));
+
+
+// Add fixtures to a group
+d.addFixtureToGroup('group', 1);
+d.addFixtureToGroup('group', 8);
+
 
 // Keys are a standardized parameter name, value is a hex value (0 - 255)
 const parameters = {
-  "intensity": 10,
+  "intensity": 0,
   "red": 255,
   "green": 255,
   "blue": 255
 };
 
-// Update the all the fixtures in the universe with the provided parameters
+
+// Update all the fixtures in the universe with the provided parameters
 d.updateAllFixtures(parameters);
+
+
+// Update all the fixtures in the group with the provided parameters
+d.updateAllFixturesInGroup('group', parameters);
--- index.js
+++ index.js
@@ -9,39 +9,85 @@ class DMXUS {
     this.driver = new Driver(port);
     this.universe = Buffer.alloc(513, 0);
     this.patch = {};
+    this.groups = {};
   }
 
 
+  // Accepts a start address and a fixture profile object, and adds the fixture to the the patch.  Returns the start address of the patched fixture.
   patchFixture(startAddress, profile) {
     this.patch[startAddress] = profile;
+    return startAddress;
   }
 
 
-  updateAllFixtures(parameters) {
-    Object.keys(this.patch).forEach(startAddress => {
-      const fixtureParameters = this.patch[startAddress].parameters;
+  // Accepts a group name and a fixture address and creates/adds the fixture to the group.
+  addFixtureToGroup(groupName, fixture) {
+    if(!this.groups[groupName]) {
+      this.groups[groupName] = [fixture];
+    }
+    else {
+      this.groups[groupName].push(fixture);
+    }
 
-      fixtureParameters.forEach((fixtureParameter, index) => {
-        this.universe[parseInt(startAddress) + index] = parameters[fixtureParameter];
-      });
+    return fixture;
+  }
+
+
+  // Updates a single fixture at the provided start address with the provided parameters.
+  updateSingleFixture(startAddress, parameters) {
+    const fixtureParameters = this.patch[startAddress].parameters;
+
+    fixtureParameters.forEach((fixtureParameter, index) => {
+      this.universe[parseInt(startAddress) + index] = parameters[fixtureParameter];
     });
 
-    this.driver.send(this.universe);
+    this.update();
   }
 
 
+  // Updates all fixtures with the provided parameters.
+  updateAllFixtures(parameters) {
+    Object.keys(this.patch).forEach(startAddress => {
+      this.updateSingleFixture(startAddress, parameters);
+    });
+
+    this.update();
+  }
+
+
+  // Updates all fixtures in the provided group name with the provided parameters.
+  updateAllFixturesInGroup(groupName, parameters) {
+    if(this.groups[groupName]) {
+      this.groups[groupName].forEach(fixtureAddress => {
+        this.updateSingleFixture(fixtureAddress, parameters);
+      });
+
+      this.update();
+    }
+  }
+
+
+  // Returns the patch
   getPatch() {
     return this.patch;
   }
 
 
+  // Sets the patch
   setPatch(patch) {
     this.patch = patch;
   }
 
 
-  static getDeviceProfile(profile) {
-    return profiles[profile];
+  // Calls the update method on the driver with the current state of the universe
+  update() {
+    this.driver.send(this.universe);
+  }
+
+
+  // Returns the device profile of the provided profile name
+  static getDeviceProfile(profileName) {
+    return profiles[profileName];
   }
 
 }
