fc1d5cdb8c31fc26ece6bd676e569eb0963f290b
Michael Hamilton <michael.hamilton@msquarehealthcare.com>
2021-03-02 13:27:28 -0500

    re-integrate dmxus web client

--- example.js
+++ example.js
@@ -13,25 +13,18 @@ d.initServer(3000);
 
 
 // Patch some fixtures
-d.patchFixture(1, dmxus.getFixtureProfile('IRGB'));
-d.patchFixture(5, dmxus.getFixtureProfile('IRGB'));
-d.patchFixture(9, dmxus.getFixtureProfile('IRGB'));
-d.patchFixture(13, dmxus.getFixtureProfile('IRGB'));
-
+d.patchFixture(1, dmxus.getFixtureProfile('RGBW'));
 
 // Add fixtures to a group
 d.addFixtureToGroup('group1', 1);
-d.addFixtureToGroup('group2', 5);
-d.addFixtureToGroup('group3', 9);
-d.addFixtureToGroup('group4', 13);
 
 
 // Keys are a standardized parameter name, value is a hex value (0 - 255)
 const initParameters = {
-  intensity: 0,
   red: 0,
   green: 0,
   blue: 0,
+  white: 0
 };
 
 
@@ -43,30 +36,9 @@ d.updateAllFixtures(initParameters);
 setInterval(() => {
   // Update all the fixtures in the group with the provided parameters
   d.updateAllFixturesInGroup('group1', {
-    intensity: 255,
     red: dmxus.getRandom8BitValue(),
     green: dmxus.getRandom8BitValue(),
     blue: dmxus.getRandom8BitValue(),
-  }, 500);
-
-  d.updateAllFixturesInGroup('group2', {
-    intensity: 255,
-    red: dmxus.getRandom8BitValue(),
-    green: dmxus.getRandom8BitValue(),
-    blue: dmxus.getRandom8BitValue(),
-  }, 500);
-
-  d.updateAllFixturesInGroup('group3', {
-    intensity: 255,
-    red: dmxus.getRandom8BitValue(),
-    green: dmxus.getRandom8BitValue(),
-    blue: dmxus.getRandom8BitValue(),
-  }, 500);
-
-  d.updateAllFixturesInGroup('group4', {
-    intensity: 255,
-    red: dmxus.getRandom8BitValue(),
-    green: dmxus.getRandom8BitValue(),
-    blue: dmxus.getRandom8BitValue(),
+    white: dmxus.getRandom8BitValue(),
   }, 500);
 }, 2000);
