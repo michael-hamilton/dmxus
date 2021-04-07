a82280ea05ce84dfd39014aa3082e8cb33b12620
Michael Hamilton <michaelhamilton626@gmail.com>
2021-04-07 15:45:21 -0400

    example.js cleanup

--- example.js
+++ example.js
@@ -9,15 +9,15 @@ const d = new dmxus('enttec-dmx-usb-pro', '/dev/tty.usbserial-EN288085');
 // const d = new dmxus('simulator', '');
 
 
-// Initialize webserver
+// Initialize webserver - default http://localhost:9090
 d.initWebServer();
 
 
 // Add devices
-d.addDevice(1, 1, dmxus.getFixtureProfile('RGBW'), 'Test Device 1', ['group1']);
-d.addDevice(2, 6, dmxus.getFixtureProfile('RGBW'), 'Test Device 2', ['group1']);
-d.addDevice(3, 11, dmxus.getFixtureProfile('RGBW'), 'Test Device 3', ['group2']);
-d.addDevice(4, 16, dmxus.getFixtureProfile('RGBW'), 'Test Device 4', ['group2']);
+d.addDevice(1, 1, dmxus.getFixtureProfile('RGBW'), 'Test Device 1', ['group']);
+d.addDevice(2, 6, dmxus.getFixtureProfile('RGBW'), 'Test Device 2', ['group']);
+d.addDevice(3, 11, dmxus.getFixtureProfile('RGBW'), 'Test Device 3', ['group']);
+d.addDevice(4, 16, dmxus.getFixtureProfile('RGBW'), 'Test Device 4', ['group']);
 
 
 // Keys are a standardized parameter name, value is a hex value (0 - 255)
@@ -33,21 +33,13 @@ const initParameters = {
 d.updateAllDevices(initParameters);
 
 
-// Update all the fixtures in 'group' every 2s with a random color
+// Loop every 5 seconds
 setInterval(() => {
   // Update all the fixtures in the group with the provided parameters
-  d.updateAllDevicesInGroup('group1', {
+  d.updateAllDevicesInGroup('group', {
     red: dmxus.getRandom8BitValue(),
     green: dmxus.getRandom8BitValue(),
     blue: dmxus.getRandom8BitValue(),
     white: dmxus.getRandom8BitValue(),
   }, 1000);
-
-  // Update all the fixtures in the group with the provided parameters
-  d.updateAllDevicesInGroup('group2', {
-    red: dmxus.getRandom8BitValue(),
-    green: dmxus.getRandom8BitValue(),
-    blue: dmxus.getRandom8BitValue(),
-    white: dmxus.getRandom8BitValue(),
-  }, 1000);
-}, 2000);
+}, 5000);
